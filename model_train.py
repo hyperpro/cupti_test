1970c0eb0c4390785a44d84ea73f042bd8414b1e
Xu Zhang <zhangxu@silver.cs.uchicago.edu>
2022-03-09 23:53:06 -0600

    add profiler

--- model_train.py
+++ model_train.py
@@ -155,9 +155,10 @@ def test(epoch):
 
 
 def trace_handler(p):
-    output = p.key_averages().table(sort_by="self_cuda_time_total", row_limit=10)
-    print(output)
-    p.export_chrome_trace("/task_trace/trace_" + str(p.step_num) + ".json")
+    # output = p.key_averages().table(sort_by="self_cuda_time_total", row_limit=10)
+    # print(output)
+    print('Trace received. Saving...')
+    p.export_chrome_trace("./task_trace/trace_" + str(p.step_num) + ".json")
 
 
 with profile(
