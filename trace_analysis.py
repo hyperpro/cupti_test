7fbd778cf40f15b2aca6d73aae12085e77c96ac3
hyperpro <zhangbjb@126.com>
2022-03-11 12:34:44 -0600

    cuda update

---
+++ trace_analysis.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import os
+import sys
+import json
+
+
+def extract_data(file_name):
+    with
+    return 0
+
+
+if __name__ == '__main__':
+    print('Trace analysis')
+    extract_data('./task_trace/trace_4.json')
